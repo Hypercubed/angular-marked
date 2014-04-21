88c81198960d211dde1d3fc134057bdba2243922
J. Harshbarger <hypercubed@gmail.com>
2014-04-21 17:04:05 +0900

    Minified

--- gruntfile.js
+++ gruntfile.js
@@ -2,22 +2,33 @@ module.exports = function(grunt){
   'use strict';
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('bower.json'),
     jshint: {
       options: { jshintrc: true },
-      all: ['gruntfile.js', '*.js']
+      all: ['gruntfile.js', '<%= pkg.main %>']
     },
     release: {
       options: {
         file: 'bower.json',
         npm: false
       }
+    },
+    uglify: {
+      options: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      src: {
+        files: {
+          'angular-marked.min.js': '<%= pkg.main %>'
+        }
+      }
     }
   });
 
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('test', ['mochaTest']);
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('publish', ['jshint','uglify','release']);
 
 };
