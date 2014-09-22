da82aa5b47f6e1bcfdc586e7cc2fb51eccd4bdc7
J. Harshbarger <hypercubed@gmail.com>
2014-09-22 15:18:46 +0900

    Improved ng-include example

--- angular-marked.js
+++ angular-marked.js
@@ -197,22 +197,16 @@
 
       ## Include a markdown file:
 
-       <example module="myApp">
+       <example module="hc.marked">
          <file name="exampleC.html">
-           <div marked ng-include="'fakeInclude.md'" />
+           <div marked ng-include="'include.html'" />
          </file>
-				<file name="exampleC.js">
-					var myApp = angular.module('myApp', ['hc.marked']);
-					myApp.run(function($templateCache) {
-					  var md = '*This* **is** [markdown](https://daringfireball.net/projects/markdown/)';
-						md += ' in a [include file][]';
-						md += '\n\n[include file]: # "Not really"';
-					  $templateCache.put('fakeInclude.md', md);
-					});
-				</file>
+				 * <file name="include.html">
+				 * *This* **is** [markdown](https://daringfireball.net/projects/markdown/) in a include file.
+				 * </file>
        </example>
    */
-   
+
   .directive('marked', ['marked', function (marked) {
     return {
       restrict: 'AE',
