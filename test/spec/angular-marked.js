cda3c7488e3d106115fb669d692362ece66b0ddf
J. Harshbarger <hypercubed@gmail.com>
2015-07-21 18:47:00 +0900

    Now handle includes using src attr, allows html in code snippits

--- test/spec/angular-marked.js
+++ test/spec/angular-marked.js
@@ -63,7 +63,7 @@ describe('Directive: marked,', function () {
     $compile,
     markdown, html;
 
-  beforeEach(inject(function ($rootScope, $templateCache, _$httpBackend_, _$compile_) {
+  beforeEach(inject(function ($rootScope, $templateCache, _$compile_) {
 
     $scope = $rootScope.$new();
 
@@ -73,35 +73,31 @@ describe('Directive: marked,', function () {
       '  Hello *world*. Here is a [link](//hello).',
       '  And an image ![alt](http://angularjs.org/img/AngularJS-large.png).',
       '',
-      '      Code goes here.'
+      '      <test>Code goes here.</test>'
     ].join('\r\n');
 
-    html = "<h1 id=\"a-heading\">A heading</h1>\n<p>Hello <em>world</em>. Here is a <a href=\"//hello\">link</a>.\nAnd an image <img src=\"http://angularjs.org/img/AngularJS-large.png\" alt=\"alt\">.</p>\n<pre><code>Code goes here.\n</code></pre>";
+    html = "<h1 id=\"a-heading\">A heading</h1>\n<p>Hello <em>world</em>. Here is a <a href=\"//hello\">link</a>.\nAnd an image <img src=\"http://angularjs.org/img/AngularJS-large.png\" alt=\"alt\">.</p>\n<pre><code>&lt;test&gt;Code goes here.&lt;/test&gt;\n</code></pre>";
 
     $scope.file = 'file.md';
-
-    $httpBackend = _$httpBackend_;
     $compile = _$compile_;
 
-    $httpBackend.expect('GET', $scope.file).respond(markdown);
+    $templateCache.put($scope.file, markdown);
 
   }));
 
   describe('Include', function () {
     it('should convert file', function () {
 
-      element = $compile('<div><div marked ng-include="file">JUNK</div></div>')($scope);
+      element = $compile('<div><div marked src="file">JUNK</div></div>')($scope);
       $scope.$digest();
-      $httpBackend.flush();
       expect(element.html()).toContain(html);
       expect(element.html()).toNotContain('JUNK');
 
     });
 
     it('should convert file', function () {
-      element = $compile('<div><div marked ng-include="\'file.md\'">JUNK</div></div>')($scope);
+      element = $compile('<div><div marked src="\'file.md\'">JUNK</div></div>')($scope);
       $scope.$digest();
-      $httpBackend.flush();
       expect(element.html()).toContain(html);
       expect(element.html()).toNotContain('JUNK');
     });
