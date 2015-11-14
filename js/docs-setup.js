933bd1d954bf61b0257e2ae465a8766cdfb2d56c
J. Harshbarger <hypercubed@gmail.com>
2015-11-14 22:26:16 +0900

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,70 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "AngularJS Markdown using marked.",
+      "keywords": "$scope angular angularjs api app class cols controller convert default defaults directive example expose function globally hc html js maincontroller markdown marked module my_markdown net ng-controller ng-model option options output overview rows service set span8 time wanted"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked",
+      "shortName": "hc.marked",
+      "type": "overview",
+      "moduleName": "hc.marked",
+      "shortDescription": "angular-marked (core module)",
+      "keywords": "adding angular angular-marked api application dependency hc html include installation js load marked module overview ready src started yourapp"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked.directive:marked",
+      "shortName": "marked",
+      "type": "directive",
+      "moduleName": "hc.marked",
+      "shortDescription": "Compiles source test into HTML.",
+      "keywords": "$scope ae angular api app bind blank block class cols compiled compiles constant content controller defaults directive evaluating examplea exampleb examplec expression file function hash hc html include js maincontroller markdown marked module my_markdown mypartialtemplate net ng-controller ng-model options opts output override quotes rows scope simple source span8 src string test text url variable view works wrap"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked.service:marked",
+      "shortName": "marked",
+      "type": "service",
+      "moduleName": "hc.marked",
+      "shortDescription": "A reference to the marked parser.",
+      "keywords": "$scope $window angular api app controller example function hc html js maincontroller marked module ng-controller parser reference service test"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked.service:markedProvider",
+      "shortName": "markedProvider",
+      "type": "service",
+      "moduleName": "hc.marked",
+      "shortDescription": "Use markedProvider to change the default behavior of the marked service.",
+      "keywords": "_blank angular api attribute behavior change config custom default displayed example examplea exampleb function gfm google google-code-prettify hc highlight highlighter href html include inline javascript js link links markdown marked markedprovider method module myappa myappb net options options-1 opts org overriding overriding-renderer-methods prettyprintone renderer return script service setoptions setrenderer syntax tables target text title true works"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.js",
+    "angular-animate.js",
+    "marked.js",
+    "angular-marked.js",
+    "script.js"
+  ],
+  "discussions": {
+    "shortName": "hypercubedgithub",
+    "url": "http://hypercubed.github.io/angular-marked/",
+    "dev": false
+  }
+};
